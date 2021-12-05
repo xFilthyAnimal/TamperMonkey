fd9afda599e68b583415fc889c609e8de537dc7d
xFilthyAnimal <51961559+xFilthyAnimal@users.noreply.github.com>
2021-12-04 20:36:40 -0600

    Add files via upload

---
+++ css.js
@@ -0,0 +1,35 @@
+var css = `
+    a:link {
+        font-family: MPlantin;
+    }
+    .cardDetails .row .label {
+        font-family: MPlantin;
+        font-size: 1.4em;
+        width: 180px;
+        padding-top: 4.5px;
+        padding-bottom: 4.5px;
+    }
+    .cardDetails .row .value {
+        font-family: MPlantin;
+        font-size: 1.1em;
+        width: 388px;
+        padding: 8px 0;
+        margin-left: 180px;
+    }
+    .cardDetails .cardtextbox {
+        padding-right: 10px;
+    }
+    .simpleRoundedBoxTitleGrey {
+        margin-top: 0;
+    }
+    tbody {
+        vertical-align: top;
+    }
+    .CommunityRatings {
+        textAlign = 'center';
+        margin = 20px 0;
+    }
+    .cardImage {
+        height: 500px;
+    }
+`;
